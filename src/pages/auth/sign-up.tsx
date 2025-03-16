cfec7a16cbdce14b5a5de56423ecd0c08296e303
Leandro Allysson <64550528+theleally@users.noreply.github.com>
2025-03-16 11:31:11 -0300

    user auth and register restaurant

--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -8,6 +8,8 @@ import { z } from "zod";
 
 import { toast } from "sonner";
 import { Link, useNavigate } from "react-router";
+import { useMutation } from "@tanstack/react-query";
+import { registerRestaurant } from "@/api/register-restaurant";
 
 const signUpForm = z.object({
   restaurantName: z.string(),
@@ -23,16 +25,23 @@ export function SignUp() {
 
   const { register, handleSubmit, formState: { isSubmitting } } = useForm<SignUpForm>()
 
+  const { mutateAsync: registerRestaurantFn } = useMutation({
+    mutationFn: registerRestaurant
+  })
+
   async function handleSignUp(data: SignUpForm) {
     try {
-      console.log(data);
-
-      await new Promise((resolve) => setTimeout(resolve, 2000))
+      await registerRestaurantFn({
+        restaurantName: data.restaurantName,
+        managerName: data.managerName,
+        email: data.email,
+        phone: data.phone
+      })
 
       toast.success("Restaurante cadastrado com sucesso!", {
         action: {
           label: "Login",
-          onClick: () => navigate("/sign-in"),
+          onClick: () => navigate(`/sign-in?email=${data.email}`),
         }
       })
       
