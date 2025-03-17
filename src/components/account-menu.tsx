3adc7e8f9b449239ab29d7fac63f5560b3fd907f
Leandro Allysson <64550528+theleally@users.noreply.github.com>
2025-03-17 15:03:12 -0300

    get profile and get restaurant

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -1,21 +1,35 @@
 import { Building, ChevronDown, LogOut } from "lucide-react";
 import { Button } from "./ui/button";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { useQuery } from "@tanstack/react-query";
+import { getProfile } from "@/api/get-profile";
+import { getManagedRestaurant } from "@/api/get-managed-restaurant";
 
 export function AccountMenu() {
+
+  const { data: profile } = useQuery({
+    queryKey: ["profile"],
+    queryFn: getProfile
+  })
+
+  const { data: managedRestaurant } = useQuery({
+    queryKey: ["managed-restaurant"],
+    queryFn: getManagedRestaurant
+  })
+  
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" className="flex items-center gap-2 select-none">
-          Pizza Shop
+          {managedRestaurant?.name}
           <ChevronDown className="w-4 h-4" />
         </Button>
       </DropdownMenuTrigger>
 
       <DropdownMenuContent align="end" className="w-56">
         <DropdownMenuLabel className="flex flex-col">
-          <span>Leandro</span>
-          <span className="text-xs font-normal text-muted-foreground">leandro@gmail.com</span>
+          <span>{ profile?.name }</span>
+          <span className="text-xs font-normal text-muted-foreground">{ profile?.email }</span>
         </DropdownMenuLabel>
 
         <DropdownMenuSeparator />
