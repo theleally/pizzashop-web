cbe8591cd51ddfc02e32a2ab06780a9672b3134d
Leandro Allysson <64550528+theleally@users.noreply.github.com>
2025-03-25 18:47:57 -0300

    get-orders

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -3,8 +3,21 @@ import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { TableCell, TableRow } from "@/components/ui/table";
 import { ArrowRight, Search, X } from "lucide-react";
 import { OrderDetails } from "./order-details";
+import { OrderStatus } from "@/components/order-status";
+import { formatDistanceToNow } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
-export function OrderTableRow() {
+export interface OrderTableRowProps {
+  order: {
+    orderId: string;
+    createdAt: string;
+    status: "pending" | "canceled" | "processing" | "delivering" | "delivered";
+    customerName: string;
+    total: number;
+  }
+}
+
+export function OrderTableRow({order}: OrderTableRowProps) {
   return (
     <TableRow>
       <TableCell>
@@ -19,17 +32,20 @@ export function OrderTableRow() {
           <OrderDetails />
         </Dialog>
      </TableCell>
-     <TableCell className="font-mono text-xs font-medium">788686fsfs1212d</TableCell>
-     <TableCell className="text-muted-foreground">há 15 minutos</TableCell>
+      <TableCell className="font-mono text-xs font-medium">{order.orderId}</TableCell>
+      <TableCell className="text-muted-foreground">{formatDistanceToNow(order.createdAt, {
+        locale: ptBR,
+        addSuffix: true,
+      })}</TableCell>
      <TableCell>
-      <div>
-        <span className="h-2 w-2 rounded-full bg-slate-400" />
-        <span className="font-mediumtext-muted-foreground">Pendente</span>
-      </div>
+      <OrderStatus status={order.status} />
      </TableCell>
-     <TableCell className="font-medium">Leandro Joo</TableCell>
+      <TableCell className="font-medium">{order.customerName}</TableCell>
      <TableCell className="font-medium">
-        R$ 149,90
+        {order.total.toLocaleString("pt-BR", {
+          style: "currency",
+          currency: "BRL"
+        })}
      </TableCell>
      <TableCell>
       <Button variant="outline" size="xs">
--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -3,8 +3,15 @@ import { Helmet } from "react-helmet-async";
 import { OrderTableRow } from "./order-table-row";
 import { OrderTableFilters } from "./order-table-filters";
 import { Pagination } from "@/components/pagination";
+import { useQuery } from "@tanstack/react-query";
+import { getOrders } from "@/api/get-orders";
 
 export function Orders() {
+  const { data: result } = useQuery({
+    queryKey: ["orders"],
+    queryFn: getOrders,
+  })
+
   return (
     <>
       <Helmet title="Pedidos" />
@@ -29,9 +36,9 @@ export function Orders() {
                 </TableRow>
               </TableHeader>
               <TableBody>
-                {Array.from({ length: 10 }).map((_, i) => {
+                {result && result.orders.map((order) => {
                   return (
-                    <OrderTableRow key={i} />
+                    <OrderTableRow key={order.orderId} order={order} />
                   )
                 })}
               </TableBody>
